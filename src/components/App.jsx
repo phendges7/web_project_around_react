693d8d39db89b365a9908d68a4ba29386153f697
phendges7 <uk_hendges@outlook.com>
2025-02-04 15:10:29 +0000

    add new features
    * implementing State Elevation
    * implementing creating and using Context
    * now user info and cards are based on contexts and being populated through API

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,34 @@
+import { useState, useEffect } from "react";
+
 import Header from "./Header/Header";
 import Main from "./Main/Main";
 import Footer from "./Footer/Footer";
 
+import api from "../utils/api";
+import CurrentUserContext from "../contexts/CurrentUserContext";
+
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    api
+      .getUserInfo()
+      .then((data) => {
+        setCurrentUser(data);
+      })
+      .catch((err) =>
+        console.error("Erro ao buscar os dados do usuário:", err)
+      );
+  }, []);
+
   return (
-    <div className="page">
-      <Header />
-      <Main />
-      <Footer />
-    </div>
+    <CurrentUserContext.Provider value={currentUser}>
+      <div className="page">
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+    </CurrentUserContext.Provider>
   );
 }
 
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,17 +1,30 @@
-import avatar from "../../images/avatarDefault.jpg";
+import { useState, useEffect, useContext } from "react";
+
+import api from "../../utils/api.js";
 import editProfileButton from "../../images/editButton.svg";
 import Popup from "./components/Popup/Popup";
 import Card from "./components/Card/Card";
 import ImagePopup from "./components/ImagePopup/ImagePopup";
-import { useState } from "react";
-import { popups, cards } from "./components/constants.jsx";
+import { popups } from "./components/constants.jsx";
+import CurrentUserContext from "../../contexts/CurrentUserContext.js";
 
 export default function Main() {
   const [popup, setPopup] = useState(null);
+  const [cards, setCards] = useState([]);
+
+  //carregar dados dos cards
+  useEffect(() => {
+    api
+      .getInitialCards()
+      .then((data) => setCards(Array.isArray(data) ? data : []))
+      .catch((err) => console.error("Erro ao buscar os cartões:", err));
+  }, []);
+
+  //carregar dados do usuário
+  const currentUser = useContext(CurrentUserContext);
 
   //manipuladores popup
   function handleOpenPopup(popup) {
-    debugger;
     setPopup(popup);
   }
 
@@ -19,11 +32,45 @@ export default function Main() {
     setPopup(null);
   }
 
+  //manipulador de curtidas
+  async function handleCardLike(card) {
+    // Verificar mais uma vez se esse cartão já foi curtido
+    const isLiked = card.isLiked;
+
+    // Enviar uma solicitação para a API e obter os dados do cartão atualizados
+    await api
+      .changeLikeCardStatus(card._id, !isLiked)
+      .then((newCard) => {
+        setCards((state) =>
+          state.map((currentCard) =>
+            currentCard._id === card._id ? newCard : currentCard
+          )
+        );
+      })
+      .catch((error) => console.error(error));
+  }
+
+  //manipulador deletar cards
+  async function handleCardDelete(card) {
+    await api
+      .deleteCard(card._id)
+      .then(() => {
+        setCards((state) =>
+          state.filter((currentCard) => currentCard._id !== card._id)
+        );
+      })
+      .catch((error) => console.error(error));
+  }
+
   return (
     <main className="content">
       <div className="profile">
         <div className="profile__picture-container">
-          <img src={avatar} alt="Foto de Perfil" className="profile__picture" />
+          <img
+            src={currentUser?.avatar}
+            alt="Foto de Perfil"
+            className="profile__picture"
+          />
           <div
             className="profile__overlay"
             onClick={() => handleOpenPopup(popups.editAvatarPopup)}
@@ -31,7 +78,7 @@ export default function Main() {
         </div>
         <div className="profile__info-wrapper">
           <div className="profile__info">
-            <h1 className="profile__name">Pedro Henrique</h1>
+            <h1 className="profile__name">{currentUser?.name}</h1>
             <img
               src={editProfileButton}
               alt="Editar Perfil"
@@ -39,7 +86,7 @@ export default function Main() {
               onClick={() => handleOpenPopup(popups.editProfilePopup)}
             />
           </div>
-          <p className="profile__description">Explorador</p>
+          <p className="profile__description">{currentUser?.about}</p>
         </div>
         <button
           aria-label="Add card"
@@ -62,6 +109,9 @@ export default function Main() {
           <Card
             key={card._id}
             card={card}
+            isLiked={card.isLiked}
+            onCardLike={handleCardLike}
+            onCardDelete={handleCardDelete}
             onImageClick={() =>
               handleOpenPopup({
                 title: "",
